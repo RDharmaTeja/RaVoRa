ac2b53dbef03c573d534a22cf299ad43c63d3bf2
R D Teja <rdarmateja@gmail.com>
2014-08-24 21:05:54 +0530

    removed unecessary

--- config.php
+++ config.php
@@ -4,7 +4,7 @@
  * */
 define('DB_NAME','ravora');
 define('DB_USER','root');
-define('DB_PASS','darmateja');
+define('DB_PASS','');
 define('DB_HOST','localhost');
 $connect = mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME);
  if (mysqli_connect_errno()){
